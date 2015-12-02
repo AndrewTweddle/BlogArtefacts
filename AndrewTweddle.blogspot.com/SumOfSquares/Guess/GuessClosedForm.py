04cde9c07d0d99f244d7c2ec070cca019d1c3da6
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2015-12-02 10:32:17 -0500

    SumOfSquares: Guess: generalize script
    - take a command line argument as the polynomial inside the summation
    over i
    - parse the argument to generate a function h(i)
    - generate coefficients based on degree of h(i)
    - display original, closed form and factorized formulae
    - fix a few mistakes in the comments

--- AndrewTweddle.blogspot.com/SumOfSquares/Guess/GuessClosedForm.py
+++ AndrewTweddle.blogspot.com/SumOfSquares/Guess/GuessClosedForm.py
@@ -1,31 +1,44 @@
+import sys
 from sympy import *
-# TODO: it's generally better to import specific functions
+from sympy.parsing.sympy_parser import parse_expr
+
+if len(sys.argv) != 2:
+  scriptName = sys.argv[0]
+  print 'USAGE: {0} "<expression>"'.format(scriptName)
+  print '<expression> is a polynomial over a variable i.'
+  print 'It is the term in a summation for values of i between 1 and n'
+  raise SystemExit(1)
+
+expr_to_parse = sys.argv[1]
+print "f(n) = SUM [{0}] for i between 1 and n".format(expr_to_parse)
 
 init_printing()
 
 # Configure symbols:
-n = Symbol("n", integer=True)
 i = Symbol("i", integer=True)
-a_0, a_1, a_2, a_3 = symbols("a_0, a_1, a_2, a_3", rational=True)
+parsed = parse_expr(expr_to_parse, {"i": i})
+h_parsed = Poly(parsed, i)
+d = h_parsed.degree(i)
 
-# Function f is the true sum of squares function. 
-# Function g is our guess as to its closed form solution.
+# Function f is the true summation function.
 def f(x):
     if x == 0:
         return 0
-    squares = [k * k for k in range(x + 1)]
+    squares = [h_parsed(k) for k in range(1, x + 1)]
     return sum(squares)
 
-# Assume the closed form solution is a degree 3 polynomial:
+# Function g is our guess as to its closed form solution.
+# Assume the closed form solution is a degree (d+1) polynomial:
+a = [Symbol("a_{0}".format(k), rational=True) for k in range(d + 2)]
+n = Symbol("n", integer=True)
 
-# h is the term inside the summation
-h_temp = Poly(n ** 2, n)
-g_temp = Poly(a_0 + a_1 * n + a_2 * n ** 2 + a_3 * n ** 3, n)
-g, h = g_temp.unify(h_temp)  # a hack to give the coefficients of f and h the same domain
-# Note that the domain is Z not Q or R, otherwise we get an error message about a_2 not being rational
-# TODO: resolve this
+g_temp = Poly(sum([a[j] * (n ** j) for j in range(d+2)]), n)
+h_temp = h_parsed.subs(i, n)
+g, h = g_temp.unify(h_temp)  # a hack to give the coefficients of g and h the same domain
+# Note that the domain is Z not Q (or R), otherwise we get an error message about a_{d+1} not being rational
+# TODO: resolve this issue
 
-# Find coefficients a_i for g such that:
+# Find coefficients a_j for g such that:
 # 1) g(0) = f(0) = 0
 eq_0 = Eq(g(0), 0)
 
@@ -42,5 +55,11 @@ p2 = Poly(p1, i)  # a hack to group terms by powers of i only
 # Equivalently each coefficient in g(i) - g(i-1) - h(i) is always zero:
 eqs = [Eq(c) for c in p2.coeffs()] + [eq_0]
 soln = solve(eqs)
-f_closed = factor(g.subs(soln))
-print(f_closed)
+
+if (soln):
+  f_closed = g.subs(soln)
+  f_closed_factorized = factor(f_closed)
+  print "     = {0}".format(f_closed)
+  print "     = {0}".format(f_closed_factorized)
+else:
+  print "NO CLOSED FORM FOUND!"
